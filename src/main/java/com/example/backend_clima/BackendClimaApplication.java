9e5fba794bb8a683e84846f70a36c959019e26c3
marwa guerfel <marwaguerfel76@gmail.com>
2023-12-07 20:19:45 +0100

    test

---
+++ src/main/java/com/example/backend_clima/BackendClimaApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_clima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendClimaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendClimaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backend_clima/BackendClimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_clima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendClimaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
